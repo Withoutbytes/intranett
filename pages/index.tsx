228a443625a487a0bb67b9743d5c431998eb4ab6
Jonathan <41968795+Withoutbytes@users.noreply.github.com>
2021-11-11 15:24:36 -0300

    removed env

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
-import { GetStaticProps } from "next";
+import { GetServerSideProps } from "next";
 
 const Index = () => {
-	return <div></div>;
+    return <div></div>;
 };
 
-export const getStaticProps: GetStaticProps = async () => {
-	return {
-		redirect: {
-			permanent: false,
-			destination: "/login",
-		},
-	};
+export const getServerSideProps: GetServerSideProps = async () => {
+    return {
+        redirect: {
+            permanent: false,
+            destination: "/login",
+        },
+    };
 };
 
 export default Index;
